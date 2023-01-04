f0d79c1fdd253f39a3436a4bf53e343b6582c53f
theMackabu <theMackabu@gmail.com>
2023-01-03 23:59:13 -0800

    fix net example formatting

--- std/examples/net.js
+++ std/examples/net.js
@@ -1,5 +1,5 @@
-let get = await http.get('https://httpbin.org/get').then((data) => data.json());
-let post = await http.post('https://httpbin.org/post', { hello: 'world' }).then((data) => data.json());
+const get = await http.get('https://httpbin.org/get').then((data) => data.json());
+const post = await http.post('https://httpbin.org/post', { hello: 'world' }).then((data) => data.json());
 
-console.log(get.pretty());
-console.log(post.pretty());
+core.print(get.pretty());
+core.print(post.pretty());
